750f89ca7a25ee13947a8a986a2644633cf5563d
Konstantin S. <havendv@gmail.com>
2026-05-07 19:03:06 +0400

    fix: make chat extension trim-safe

--- src/libs/Coze/Extensions/CozeClient.ChatClient.cs
+++ src/libs/Coze/Extensions/CozeClient.ChatClient.cs
@@ -2,6 +2,7 @@
 
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Text.Json;
 using Microsoft.Extensions.AI;
 
 namespace Coze;
@@ -331,7 +332,7 @@ public partial class CozeClient : IChatClient
                     var resultText = result.Result switch
                     {
                         string s => s,
-                        not null => System.Text.Json.JsonSerializer.Serialize(result.Result),
+                        not null => SerializeValue(result.Result),
                         _ => result.Exception?.Message ?? string.Empty,
                     };
 
@@ -354,7 +355,7 @@ public partial class CozeClient : IChatClient
                 foreach (var fc in functionCalls)
                 {
                     var argsJson = fc.Arguments is not null
-                        ? System.Text.Json.JsonSerializer.Serialize(fc.Arguments)
+                        ? SerializeArguments(fc.Arguments)
                         : "{}";
 
                     additionalMessages.Add(new global::Coze.EnterMessage2
@@ -416,6 +417,90 @@ public partial class CozeClient : IChatClient
         }
 
         return request;
+    }
+
+    private static string SerializeArguments(IEnumerable<KeyValuePair<string, object?>> arguments)
+    {
+        using var stream = new MemoryStream();
+        using (var writer = new Utf8JsonWriter(stream))
+        {
+            WriteObject(writer, arguments);
+        }
+
+        return Encoding.UTF8.GetString(stream.ToArray());
+    }
+
+    private static string SerializeValue(object value)
+    {
+        using var stream = new MemoryStream();
+        using (var writer = new Utf8JsonWriter(stream))
+        {
+            WriteValue(writer, value);
+        }
+
+        return Encoding.UTF8.GetString(stream.ToArray());
+    }
+
+    private static void WriteObject(Utf8JsonWriter writer, IEnumerable<KeyValuePair<string, object?>> values)
+    {
+        writer.WriteStartObject();
+        foreach (var (key, value) in values)
+        {
+            writer.WritePropertyName(key);
+            WriteValue(writer, value);
+        }
+        writer.WriteEndObject();
+    }
+
+    private static void WriteValue(Utf8JsonWriter writer, object? value)
+    {
+        switch (value)
+        {
+            case null:
+                writer.WriteNullValue();
+                break;
+            case JsonElement element:
+                element.WriteTo(writer);
+                break;
+            case string text:
+                writer.WriteStringValue(text);
+                break;
+            case bool boolean:
+                writer.WriteBooleanValue(boolean);
+                break;
+            case int number:
+                writer.WriteNumberValue(number);
+                break;
+            case long number:
+                writer.WriteNumberValue(number);
+                break;
+            case float number:
+                writer.WriteNumberValue(number);
+                break;
+            case double number:
+                writer.WriteNumberValue(number);
+                break;
+            case decimal number:
+                writer.WriteNumberValue(number);
+                break;
+            case IReadOnlyDictionary<string, object?> dictionary:
+                WriteObject(writer, dictionary);
+                break;
+            case IDictionary<string, object?> dictionary:
+                WriteObject(writer, dictionary);
+                break;
+            case IEnumerable<object?> items:
+                writer.WriteStartArray();
+                foreach (var item in items)
+                {
+                    WriteValue(writer, item);
+                }
+                writer.WriteEndArray();
+                break;
+            default:
+                writer.WriteStringValue(value.ToString());
+                break;
+        }
     }
 
     private string GetBotId(ChatOptions? options)
